b72e7ab2601534b69b12ead9cf7de70d32546c5c
Propeex <60839060+Propeex@users.noreply.github.com>
2026-07-29 20:07:20 +0900

    메인 창 표시 이름을 타르코프헬퍼_JH로 변경

---
+++ TarkovHelper/MainWindow.ReleaseIdentity.cs
@@ -0,0 +1,10 @@
+namespace TarkovHelper;
+
+public partial class MainWindow
+{
+    protected override void OnInitialized(EventArgs e)
+    {
+        base.OnInitialized(e);
+        Title = "타르코프헬퍼_JH";
+    }
+}
